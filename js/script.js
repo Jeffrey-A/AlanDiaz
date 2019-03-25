12ffd9ae77b6283fd8f8c34219067cdbbfc63d80
Jeffrey Almanzar <almanzarortizjeffrey@gmail.com>
2019-03-25 09:42:34 -0400

    type js added

--- js/script.js
+++ js/script.js
@@ -4,4 +4,12 @@ $(document).ready(function(){
         play:2500,
         pagination:false
     });
+
+    var typed = new Typed('.typed', {
+        strings: ['Certified Driving Instructor.', 'Specialized in  training new and nervous people.'],
+        typeSpeed:60,
+        loop:true,
+        startDelay:1000, //wait one second to start
+        showCursor:false
+      });
 });
